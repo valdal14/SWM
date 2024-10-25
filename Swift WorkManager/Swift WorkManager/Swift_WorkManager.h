72952a62a5a99f3ab337890441ccd60b39cea2ff
Valerio DAlessio <u105550@canda.com>
2024-10-25 14:47:50 +0200

    initial package

---
+++ Swift WorkManager/Swift WorkManager/Swift_WorkManager.h
@@ -0,0 +1,18 @@
+//
+//  Swift_WorkManager.h
+//  Swift WorkManager
+//
+//  Created by Valerio D'ALESSIO on 25/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swift_WorkManager.
+FOUNDATION_EXPORT double Swift_WorkManagerVersionNumber;
+
+//! Project version string for Swift_WorkManager.
+FOUNDATION_EXPORT const unsigned char Swift_WorkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_WorkManager/PublicHeader.h>
+
+

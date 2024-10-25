344dfae4961d6bc39812f4440409111569b2c1d0
Valerio DAlessio <u105550@canda.com>
2024-10-25 15:49:03 +0200

    renamed package

---
+++ SWM/SWM/SWM.h
@@ -0,0 +1,18 @@
+//
+//  SWM.h
+//  SWM
+//
+//  Created by Valerio D'ALESSIO on 25/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWM.
+FOUNDATION_EXPORT double SWMVersionNumber;
+
+//! Project version string for SWM.
+FOUNDATION_EXPORT const unsigned char SWMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWM/PublicHeader.h>
+
+
---
+++ SWM/SWMTests/SWMTests.swift
@@ -0,0 +1,13 @@
+//
+//  SWMTests.swift
+//  SWMTests
+//
+//  Created by Valerio D'ALESSIO on 25/10/24.
+//
+
+import XCTest
+import SWM
+
+final class SWMTests: XCTestCase {
+
+}
